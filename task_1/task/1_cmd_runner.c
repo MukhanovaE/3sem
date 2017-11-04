106a0c7f912b49b84960cc2d3bb191dc57bd926b
MukhanovaE <32328891+MukhanovaE@users.noreply.github.com>
2017-11-05 00:16:48 +0300

    waitpid added

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -1,11 +1,53 @@
-/*
-  Write a program which allow you to: 
-- run another programs via command line.
-- get exit codes of terminated programs
-
-## TIPS:
-1. Use "2_fork_wait_exit.c" and "4_exec_dir.c" from examples. Combine them.
-2. Parse input string according to the type of exec* (see "man exec").
-   a) if execvp is used, string splitting into "path" and "args" is all you need.
-3. Collect exit codes via waitpid/WEXITSTATUS.
-*/
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+int main() 
+{    
+    pid_t pid;    
+    int status;    
+    char path[1000];    
+    char *arg[100];     
+    char line[1000];   
+    char arrays[100][100]={""} ; 
+    while(1) 
+    {        
+        int i = 0, j = 0, k=0, pos=0;   
+        fgets(line, 1000, stdin);  
+        int lenght = strlen(line); 
+        if (lenght == 1)        
+        {           
+            return -1;       
+        }        
+        for (i = 0; i < lenght; i++)
+        {           
+            if (line[i] == ' '|| line[i] == '\0' || line[i] == '\n')      
+            {               
+                arrays[j][i-pos] = '\0';     
+                pos=i+1;           
+                arg[j]=arrays[j];   
+                j++;            
+                if(j>=99)  break ;
+            }            
+            else            
+            {              
+                arrays[j][i-pos] = line[i];  
+            }       
+            
+        }
+        arg[j]=NULL; 
+        switch(pid = fork()) 
+        {
+            case -1:
+                printf("fork error\n");
+            case 0:
+                execvp(arrays[0], arg);   
+            default:
+                wait(&status); 
+                printf("Ret code: %d\n", WEXITSTATUS(status));
+        } 
+    }
+    return 0;
+    
+}
