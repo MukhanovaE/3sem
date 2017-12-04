86c231e5b8d292e9ba080705fcc1a017986bad5d
MukhanovaE <lilevy@list.ru>
2017-12-04 14:57:09 +0300

    Realisation of MC integral with fork

--- task_3/task.c
+++ task_3/task.c
@@ -12,3 +12,95 @@
   б*) определить, при каких n' (насколько больших) накладные расходы от использования многопоточности [и синхронизации, если она имела место]
   превосходят преимущества их использования.
 */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double intergalMC(long NTry) ;
+
+int main() 
+{
+    int fd[2], result, proc=0;
+    const int N = 4; //number of processes
+    pid_t child[N];
+    double childRes[N];
+    const long Ntry=10000000;  
+    size_t size;
+    double arr[2];
+    double arr1[2]={1.,2.};
+    char resstring[14];
+    int parentPID = getpid() ; 
+    
+    
+    if(pipe(fd) < 0)
+    {
+        printf("1: Can not create pipe\n");
+        exit(-1);
+    }
+    
+    for (proc = 0; proc < N; proc++)
+    {
+       if(getpid()==parentPID) 
+        result = fork();
+        child[proc]=result;
+     }
+     
+     //Calculate integral
+     double intResult=intergalMC(Ntry);
+     
+     //send result to parent
+     if(getpid()!=parentPID){
+       close(fd[0]);
+       size = write(fd[1], &intResult, sizeof(double));
+       if (size < 0) 
+       {
+         printf("Can\'t write\n");
+         exit(-1);
+       }
+       close (fd[1]);
+       exit(-1);
+     }
+     
+     //parent
+     double total=0.;
+     //collect from children 
+     if(getpid()==parentPID){
+       for (proc = 0; proc < N; proc++){
+          waitpid( child[proc], &result,0); 
+          close(fd[1]);
+          size = read(fd[0], childRes, sizeof(double));
+          total+=childRes[0];
+          if (size !=sizeof(double))
+          {
+                        printf("Can\'t read\n");
+                        exit(-1);
+          }
+       }
+     }
+     total/=N*Ntry;
+     printf("Integral=%e \n",total) ;
+     
+  return 0;
+}
+
+double intergalMC(long Ntry)
+{
+  //calculate Monte-Carlo integral  
+  double xMin=0.;
+  double xMax=2.;
+  double yMin=0.;
+  double yMax=2.;
+  double res=0.;  
+  for(long i=0; i<Ntry; i++){
+    double x=xMin+(xMax-xMin)*rand()/RAND_MAX  ;
+    double y=yMin+(yMax-yMin)*rand()/RAND_MAX  ;
+  
+    double f = x*y;
+    res+=f ;
+  }
+  return res ;
+}
